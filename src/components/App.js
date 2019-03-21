e7c2268dd799bda8f5a8b8fa144517b1873bcfda
Remco Zwartepoorte <remco.zwartepoorte@gmail.com>
2019-03-21 15:25:33 +0100

    HashRouter vs BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { createGlobalStyle } from 'styled-components';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 
 import '../fonts/gilroy.css';
